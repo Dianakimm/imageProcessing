bb1fb03ae008d053c4477606b4678ddc18093ba0
tulipksh <tulipksh@gmail.com>
2023-03-28 17:41:38 +0900

    커밋2

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ import cv2
 ##영상소스열기
 src = cv2.imread('image/a.png')
 
+##영상 처리 알고리즘 구현
+dst1 = cv2.cvtColor(src,cv2.COLOR_RGB2GRAY)
+
+
+
+
+
+
+
+
+
 ##영상 디스플레이
 cv2.imshow('src', src)
+cv2.imshow('dst1',dst1)
 
 ##마무리
 cv2.waitKey(0)
